2591fbaa085c92b7fd81d9dc6a4a7794c5dd757b
samfeldman824 <sam@feldman.com>
2023-04-10 13:49:05 -0400

    new key map and csv template

--- chart_parser.py
+++ chart_parser.py
@@ -2,25 +2,25 @@
 import pandas as pd
 
 shot_dictionary = {
-    "f": ["Shot Type", "forehand"],
-    "b": ["Shot Type", "backhand"],
-    "r": ["Shot Type", "fh slice"],
-    "s": ["Shot Type", "bh slice"],
-    "v": ["Shot Type", "fh volley"],
-    "z": ["Shot Type", "bh volley"],
-    "o": ["Shot Type", "overhead"],
+    "j": ["Shot Type", "forehand"],
+    "k": ["Shot Type", "backhand"],
+    "u": ["Shot Type", "fh slice"],
+    "i": ["Shot Type", "bh slice"],
+    "o": ["Shot Type", "fh volley"],
+    "p": ["Shot Type", "bh volley"],
+    "l": ["Shot Type", "overhead"],
     "*": ["Rally Ending", "Winner"],
     "@": ["Rally Ending", "Unforced Error"],
     "#": ["Rally Ending", "Forced Error"],
     "!": ["Rally Ending", "Fault"],
-    "1": ["Shot Direction", "cross"],
+    "1": ["Shot Direction", "line"],
     "2": ["Shot Direction", "middle"],
-    "3": ["Shot Direction", "line"],
+    "3": ["Shot Direction", "cross"],
     "4": ["Shot Direction", "inside-out"],
     "5": ["Shot Direction", "inside-in"],
-    "6": ["Shot Direction", "wide"],
+    "6": ["Shot Direction", "t"],
     "7": ["Shot Direction", "body"],
-    "8": ["Shot Direction", "t"],
+    "8": ["Shot Direction", "wide"],
     "w": ["Location", "wide"],
     "d": ["Location", "deep"],
     "n": ["Location", "net"]
@@ -29,8 +29,8 @@ shot_dictionary = {
 
 string1 = "6b3b1f3b2f2f1f3b3b3s3b3b3b3s3b1f2b3s2f1n@"
 string2 = "7b3b2f4s2s3v3o5z2f4d@"
-shots_list = ["f", "b", "r", "s", "v", "z", "o"]
-players = ['Sam', 'Roger']
+shots_list = ["j", "k", "u", "i", "o", "p", "l"]
+players = ['Sam Feldman', 'Corey Craig']
 
 def parse_point(point: str, shots_list: list) -> list:
     substrings = []
@@ -68,15 +68,16 @@ def parse_point(point: str, shots_list: list) -> list:
 
 # print(parse_point(my_string, shots_list))  # Output: ['f3d@', 'b3n']
 
-def parse_shots(shots: list, shot_options: dict, first_or_second: str, players: list, server: str):
-    headers = ['Shot Type', 'Shot Direction', 'Rally Ending', 'Location', 'Return', 'Player']
+def parse_shots(shots: list, shot_options: dict, first_or_second: str, players: list, server: str, score: str):
+    headers = ['Shot Type', 'Shot Direction', 'Rally Ending', 'Location', 'Return', 'Player', 'Score']
     df = pd.DataFrame(columns=headers)
 
     table_i = 0
 
     if players[0] != server:
-        players[0] = players[1]
-        players[1] = server
+        players[1] = players[0]
+        players[0] = server
+        
 
     for index, shot in enumerate(shots):
         if index % 2 == 0:
@@ -84,6 +85,7 @@ def parse_shots(shots: list, shot_options: dict, first_or_second: str, players: 
         else:
             df.at[table_i, 'Player'] = players[1]
 
+        
 
         # print(shot)
         if index == 0:
@@ -91,6 +93,12 @@ def parse_shots(shots: list, shot_options: dict, first_or_second: str, players: 
             mapped_serve = shot_options[serve]
             df.at[table_i, 'Shot Direction'] = mapped_serve[1]
             df.at[table_i, 'Shot Type'] = first_or_second + ' serve'
+            if first_or_second == 'first':
+                df.at[table_i, 'Score'] = score
+            else:
+                df.at[table_i, 'Score'] = ''
+        else:
+            df.at[table_i, 'Score'] = ''
         
 
         if index == 1:
@@ -113,20 +121,25 @@ def parse_shots(shots: list, shot_options: dict, first_or_second: str, players: 
     return df
     # print(df)
 
-def parse_match(filepath: str, players: list):
+def parse_match(filepath: str):
     match_df = pd.DataFrame()
     df = pd.read_csv(filepath)
-    for index, row in df[["Server", "1st", "2nd"]].iterrows():
+    players_column = df['Players'].to_list()
+    players = [players_column[0], players_column[1]]
+
+    for index, row in df[["Server", "Score", "1st", "2nd", ]].iterrows():
         point_df = pd.DataFrame()
         point_df2 = pd.DataFrame()
         server = str(row.values.tolist()[0])
-        first_serve = str(row.values.tolist()[1])
+        game_score = str(row.values.tolist()[1])
+        first_serve = str(row.values.tolist()[2])
+        second_serve = str(row.values.tolist()[3])
         shots = parse_point(first_serve, shots_list)
-        point_df = parse_shots(shots, shot_dictionary, 'first', players, server)
-        second_serve = str(row.values.tolist()[2])
+        point_df = parse_shots(shots, shot_dictionary, 'first', players, server, game_score)
+
         if second_serve != 'nan':
             shots = parse_point(second_serve, shots_list)
-            point_df2 = parse_shots(shots, shot_dictionary, 'second', players, server)
+            point_df2 = parse_shots(shots, shot_dictionary, 'second', players, server, game_score)
         
         point_df = pd.concat([point_df, point_df2], axis=0)
         point_df = point_df.reset_index(drop=True)
@@ -150,6 +163,6 @@ def parse_match(filepath: str, players: list):
 
 # print(parse_point("7w*f4b2s4", shots_list))
 # print(parse_point("7n", shots_list))
-match_df = parse_match('./csvs/match2.csv', players)
+match_df = parse_match('./csvs/corey.csv')
 print(match_df)
 # print(parse_point("7b3f3b2s3f3*", shots_list))
