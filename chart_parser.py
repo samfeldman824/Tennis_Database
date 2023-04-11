4982ab77d849c0eb0303fbe17e10c34263db3d98
samfeldman824 <sam@feldman.com>
2023-04-10 21:08:44 -0400

    documentation for chart_parser

--- chart_parser.py
+++ chart_parser.py
@@ -1,6 +1,8 @@
 # pylint: disable-all
 import pandas as pd
 
+
+# Dictionary with mappings for each shot
 shot_dictionary = {
     "j": ["Shot Type", "forehand"],
     "k": ["Shot Type", "backhand"],
@@ -30,17 +32,29 @@ shot_dictionary = {
     "n": ["Location", "net"]
 }
 
+# Fills shots_list with with all characters that are shots so point string can be separated into shots
+shots_list = []
+for key, value in shot_dictionary.items():
+    if value[0] == 'Shot Type':
+        shots_list.append(key)
 
-string1 = "6b3b1f3b2f2f1f3b3b3s3b3b3b3s3b1f2b3s2f1n@"
-string2 = "7b3b2f4s2s3v3o5z2f4d@"
-shots_list = ["j", "k", "u", "i", "o", "p", "l", "n", "m", "g", "h"]
-players = ['Sam Feldman', 'Corey Craig']
 
 def parse_point(point: str, shots_list: list) -> list:
+    """
+    Parse a point string and extract substrings based on a list of shots.
+
+    Args:
+        point (str): The point string to be parsed.
+        shots_list (list): A list of shots to be used for parsing.
+
+    Returns:
+        list: A list of substrings extracted from the point string.
+    """
+    
     substrings = []
     current_substring = ""
-    # substrings.append(point[0])
 
+    # Check if point doesn not contain any shots other than serve
     only_serve = True
     char_index = 0
     for char in point:
@@ -49,49 +63,64 @@ def parse_point(point: str, shots_list: list) -> list:
             break
         char_index += 1
     
+    # If point has shots after serve, parse and split them into shots
     if not only_serve:
         substrings.append(point[0:char_index])
         i = char_index
         while i < len(point):
             char = point[i]
             if char in shots_list:
-                # Add the current character and the characters after it until the next "f" or "b"
+                # Add the current character and the characters after it until the next shot character
                 j = i + 1
                 while j < len(point) and point[j] not in shots_list:
                     j += 1
                 current_substring = point[i:j]
                 # Add the current substring to the list
                 if current_substring != "":
-                    # print(current_substring)
                     substrings.append(current_substring)
                 i = j
-        # substrings.pop(0)
         return substrings
+    # If point only contained a serve
     else:
         return [point]
 
-# print(parse_point(my_string, shots_list))  # Output: ['f3d@', 'b3n']
+def parse_shots(shots: list, shot_options: dict, first_or_second: str, players: list, server: str, score: str) -> pd.DataFrame():
+    """
+    Parses each shot and adds the corresponding details to a Pandas Dataframe
 
-def parse_shots(shots: list, shot_options: dict, first_or_second: str, players: list, server: str, score: str):
+    Args:
+        shots (list): List of all separated shots to be parsed
+        shot_options (dict): Dictionary used to map each character to certain stat
+        first_or_second (str): Whether it is first or second serve
+        players (list): List with the names of both players
+        server (str): Name of server of the point
+        score (str): Score at beginning of point
+
+    Returns:
+        pd.Dataframe(): A dataframe containing each shot and corresponding details
+
+    """
+
+
+    # Header names for df
     headers = ['Shot Type', 'Shot Direction', 'Rally Ending', 'Location', 'Return', 'Player', 'Score']
     df = pd.DataFrame(columns=headers)
 
     table_i = 0
 
+    # Have the server's name first in the list of players
     if players[0] != server:
         players[1] = players[0]
         players[0] = server
         
-
+    # Alternate the player name for each shot
     for index, shot in enumerate(shots):
         if index % 2 == 0:
             df.at[table_i, 'Player'] = players[0]
         else:
             df.at[table_i, 'Player'] = players[1]
 
-        
-
-        # print(shot)
+        # If first shot of point, it is a serve
         if index == 0:
             serve = shot[0]
             mapped_serve = shot_options[serve]
@@ -104,11 +133,13 @@ def parse_shots(shots: list, shot_options: dict, first_or_second: str, players: 
         else:
             df.at[table_i, 'Score'] = ''
         
-
+        # If second shot of point, it is a return
         if index == 1:
             df.at[table_i, 'Return'] = True
         else:
             df.at[table_i, 'Return'] = False
+
+        # Map each character to a stat of the shot
         i = 0
         while i < len(shot):
             char = shot[i]
@@ -119,54 +150,55 @@ def parse_shots(shots: list, shot_options: dict, first_or_second: str, players: 
             i += 1
         table_i += 1
 
+    # Fill missing values in 'Location' and 'Rally Ending' columns with 'in'
     df['Location'].fillna('in', inplace=True)
     df['Rally Ending'].fillna('in', inplace=True)
 
     return df
-    # print(df)
 
 def parse_match(filepath: str):
-    match_df = pd.DataFrame()
-    df = pd.read_csv(filepath)
-    players_column = df['Players'].to_list()
-    players = [players_column[0], players_column[1]]
+    """
+    Parses a match data file and creates a Pandas DataFrame with the parsed data.
 
-    for index, row in df[["Server", "Score", "1st", "2nd", ]].iterrows():
-        point_df = pd.DataFrame()
-        point_df2 = pd.DataFrame()
-        server = str(row.values.tolist()[0])
-        game_score = str(row.values.tolist()[1])
-        first_serve = str(row.values.tolist()[2])
-        second_serve = str(row.values.tolist()[3])
-        shots = parse_point(first_serve, shots_list)
-        point_df = parse_shots(shots, shot_dictionary, 'first', players, server, game_score)
+    Args:
+        filepath (str): Filepath of the match data file.
 
+    Returns:
+        pd.DataFrame: A DataFrame containing the parsed match data.
+
+    """
+
+
+
+    match_df = pd.DataFrame()  # Initialize an empty DataFrame to store the parsed match data
+    df = pd.read_csv(filepath)  # Read the match data from the CSV file into a DataFrame
+    players_column = df['Players'].to_list()  # Extract the 'Players' column from the DataFrame and convert it to a list
+    players = [players_column[0], players_column[1]]  # Extract the names of the players from the 'Players' column
+
+
+    # Iterate through each row in the DataFrame and parse the match data
+    for index, row in df[["Server", "Score", "1st", "2nd"]].iterrows():
+        point_df = pd.DataFrame()  # Initialize an empty DataFrame to store the parsed point data
+        point_df2 = pd.DataFrame()  # Initialize an empty DataFrame to store the parsed point data for the second serve
+
+        server = str(row.values.tolist()[0])  # Extract the server's name from the row
+        game_score = str(row.values.tolist()[1])  # Extract the game score from the row
+        first_serve = str(row.values.tolist()[2])  # Extract the first serve data from the row
+        second_serve = str(row.values.tolist()[3])  # Extract the second serve data from the row
+
+        shots = parse_point(first_serve, shots_list)  # Parse the first serve data to obtain a list of shots
+        point_df = parse_shots(shots, shot_dictionary, 'first', players, server, game_score)  # Parse the shots and create a DataFrame for the first serve
+
+        # If there is second serve data, parse it and create a DataFrame for the second serve
         if second_serve != 'nan':
-            shots = parse_point(second_serve, shots_list)
-            point_df2 = parse_shots(shots, shot_dictionary, 'second', players, server, game_score)
-        
-        point_df = pd.concat([point_df, point_df2], axis=0)
-        point_df = point_df.reset_index(drop=True)
-        match_df = pd.concat([match_df, point_df], axis=0)
-    return match_df
+            shots = parse_point(second_serve, shots_list)  # Parse the second serve data to obtain a list of shots
+            point_df2 = parse_shots(shots, shot_dictionary, 'second', players, server, game_score)  # Parse the shots and create a DataFrame for the second serve
 
-# match_df = parse_match('/Users/samfeldman/Desktop/Tennis_Database/points_sheet.csv')
-# print(match_df)
+        point_df = pd.concat([point_df, point_df2], axis=0)  # Concatenate the point data for first and second serves
+        point_df = point_df.reset_index(drop=True)  # Reset the index of the point data DataFrame
+        match_df = pd.concat([match_df, point_df], axis=0)  # Concatenate the point data to the match data
 
+    return match_df # Return the final parsed match data DataFrame
 
-# shots_list1 = parse_point(string1, shots_list)
-# shots_list2 = parse_point(string2, shots_list)
-# # shots_list = ['8', 'f3', 'f4', 'b1', 's3', 'z3', 'o1*']
-# df1 = parse_shots(shots_list1, shot_dictionary, ['Sam Feldman', 'Roger Chou'])
-# df2 = parse_shots(shots_list2, shot_dictionary, ['Sam Feldman', 'Roger Chou'])
-
-# combined_df = pd.concat([df1, df2], axis=0)
-# combined_df = combined_df.reset_index(drop=True)
-# print(combined_df)
-
-
-# print(parse_point("7w*f4b2s4", shots_list))
-# print(parse_point("7n", shots_list))
 match_df = parse_match('./csvs/corey.csv')
 print(match_df)
-# print(parse_point("7b3f3b2s3f3*", shots_list))
